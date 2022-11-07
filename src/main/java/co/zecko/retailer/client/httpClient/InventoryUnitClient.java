f707d9fbecd545397e311b60259acb5dcacb06b0
Jai Malani <71269258+jai38@users.noreply.github.com>
2022-11-07 17:04:30 +0530

    requested changes added

--- src/main/java/co/zecko/retailer/client/httpClient/InventoryUnitClient.java
+++ src/main/java/co/zecko/retailer/client/httpClient/InventoryUnitClient.java
@@ -6,6 +6,7 @@ import co.zecko.retailer.common.pojo.inventoryUnit.InventoryUnitParams;
 import co.zecko.retailer.common.pojo.inventoryUnit.InventoryUnitResponseWrapper;
 import co.zecko.retailer.exception.ZeckoException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
@@ -27,7 +28,7 @@ public class InventoryUnitClient extends BaseClient {
         String routeUri = "";
 
         if (StringUtils.isEmpty(id)) {
-            String message = "Missing required parameter inventoryUnitId";
+            String message = "Missing required parameter id";
             throw new ZeckoException(message, HttpStatus.BAD_REQUEST);
         }
         routeUri = String.format("/%s/track", id);
@@ -42,9 +43,15 @@ public class InventoryUnitClient extends BaseClient {
         Map<String, String> headers = getBaseHeaders(zeckoAccessToken);
         String routeUri = "";
         if (StringUtils.isEmpty(id)) {
-            String message = "Missing required parameter inventoryUnitId";
+            String message = "Missing required parameter id";
             throw new ZeckoException(message, HttpStatus.BAD_REQUEST);
         }
+
+        if(ObjectUtils.isEmpty(inventoryUnitParams)){
+            String message = "Missing required parameter inventoryUnitParams";
+            throw new ZeckoException(message, HttpStatus.BAD_REQUEST);
+        }
+
         routeUri = String.format("/%s/cancel", id);
 
         String url = getUrl(BASE_URI + routeUri);
